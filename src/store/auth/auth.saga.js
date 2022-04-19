2b8d459e7d80eb0bc10498e9cfdf96cf8693d67a
Vu Le <1859049@itec.hcmus.edu.vn>
2022-04-19 16:44:25 +0700

    auth

--- src/store/auth/auth.saga.js
+++ src/store/auth/auth.saga.js
@@ -1,5 +1,5 @@
-import { call, put, takeLatest, putResolve } from "redux-saga/effects";
-import { actionFetchLogin, actionLogout, authActions } from ".";
+import { call, put, takeLatest, putResolve, delay } from "redux-saga/effects";
+import { actionFetchLogin, actionFetchRegister, actionLogout, authActions } from ".";
 import { actionFetchUser } from "../user";
 import { authService } from "../../services/authService"
 import { clearToken, clearUser, setToken } from "../../utils/token"
@@ -7,17 +7,19 @@ import { clearToken, clearUser, setToken } from "../../utils/token"
 function* fetchLogin(action) {
     try {
         // console.log('fetch login', action);
-        yield putResolve(authActions.statusFetchLogin(true))
+        // yield putResolve(authActions.statusFetchLogin(true))
 
         const res = yield call(authService.login, action.payload)
         // console.log('res', res);
         if (res.message) {
-            return yield put(authActions.errorMessage(res.message))
+            // return yield put(authActions.errorMessage(res.message))
+            return action.payload?.error?.(res.message)
         }
 
         setToken(res.data)
         yield put(actionFetchUser())
 
+        action.payload?.success?.()
         // const user = yield call(userService.getInfo)
         // setUser(user.data)
 
@@ -26,7 +28,8 @@ function* fetchLogin(action) {
     } catch (error) {
         console.log(error);
     } finally {
-        yield put(authActions.statusFetchLogin(false))
+        action.payload?.end?.()
+        // yield put(authActions.statusFetchLogin(false))
     }
 }
 
@@ -35,7 +38,26 @@ function* logout() {
     clearUser()
 }
 
+function* fetchRegister(action) {
+    try {
+        const res = yield call(authService.register, action.payload.data)
+        if(res.error){
+            return action.payload?.error?.(res.error)
+        }
+
+        setToken(res.data)
+        yield put(actionFetchUser())
+
+        action.payload?.success?.()
+    } finally {
+        action.payload?.end?.()
+    }
+}
+
 export function* authSaga() {
     yield takeLatest(actionFetchLogin, fetchLogin)
     yield takeLatest(actionLogout, logout)
+
+    yield takeLatest(actionFetchRegister, fetchRegister)
+
 }
